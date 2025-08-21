65cb5ff7d7ffdca685bf2acb4cefab6cab4b7461
slmey <p3l00ts@gmail.com>
2025-08-21 16:35:20 +0200

    migrated all of my files into the right locations

--- LPR381_Solver/LPR381_Solver/Algorithms/CuttingPlane.cs
+++ LPR381_Solver/LPR381_Solver/Algorithms/CuttingPlane.cs
@@ -1,12 +1,81 @@
-﻿using System;
-using System.Collections.Generic;
+using System;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace LPR381_Solver.Algorithms
 {
-    internal class CuttingPlane
+    public class CuttingPlane
     {
+        private readonly RevisedSimplex _lpSolver;
+        private readonly IIterationLogger _log;
+
+        public CuttingPlane(RevisedSimplex lpSolver, IIterationLogger logger)
+        {
+            _lpSolver = lpSolver;
+            _log = logger;
+        }
+
+        public SolveResult Solve(CanonicalForm cf)
+        {
+            _log.LogHeader("Cutting Plane (Gomory)");
+
+            var intMask = cf.VariableTypes.Select(t => t == VarType.Int || t == VarType.Bin).ToArray();
+            var current = cf.Clone();
+
+            for (int iter = 1; iter <= 50; iter++)
+            {
+                var lp = _lpSolver.Solve(current);
+                if (lp.Status != "Optimal")
+                {
+                    _log.Log($"LP status: {lp.Status}. Abort.");
+                    return lp;
+                }
+
+                int fracIndex = -1;
+                for (int j = 0; j < current.N; j++)
+                {
+                    if (intMask[j])
+                    {
+                        var frac = Math.Abs(lp.X[j] - Math.Round(lp.X[j]));
+                        if (frac > 1e-6) { fracIndex = j; break; }
+                    }
+                }
+                if (fracIndex == -1)
+                {
+                    lp.Status = "Optimal (Integer)";
+                    _log.Log($"All integers integral after {iter-1} cuts. Z = {lp.Objective:0.###}");
+                    return lp;
+                }
+
+                double rhs = Math.Floor(lp.X[fracIndex]);
+                _log.Log($"Cut {iter}: x{fracIndex+1} <= {rhs} (from fractional {lp.X[fracIndex]:0.###})");
+
+                var A2 = new double[current.M + 1, current.N];
+                for (int i = 0; i < current.M; i++)
+                    for (int j = 0; j < current.N; j++)
+                        A2[i, j] = current.A[i, j];
+                A2[current.M, fracIndex] = 1.0;
+
+                var b2 = new double[current.M + 1];
+                Array.Copy(current.b, b2, current.M);
+                b2[current.M] = rhs;
+
+                var signs2 = new ConstraintSign[current.M + 1];
+                Array.Copy(current.Signs, signs2, current.M);
+                signs2[current.M] = ConstraintSign.LE;
+
+                current = new CanonicalForm
+                {
+                    Sense = cf.Sense,
+                    A = A2,
+                    b = b2,
+                    c = cf.c.ToArray(),
+                    Signs = signs2,
+                    VariableTypes = cf.VariableTypes.ToArray(),
+                    VariableNames = cf.VariableNames?.ToArray()
+                };
+            }
+
+            return new SolveResult { Status = "CutLimit" };
+        }
     }
 }
--- LPR381_Solver/LPR381_Solver/Algorithms/PrimalSimplex.cs
+++ LPR381_Solver/LPR381_Solver/Algorithms/PrimalSimplex.cs
@@ -1,12 +1,83 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace LPR381_Solver.Algorithms
+namespace LPR381.Core
 {
-    internal class PrimalSimplex
+    public class PrimalSimplex
     {
+        private readonly IIterationLogger _log;
+        private readonly double _M;
+        private readonly double _eps;
+        public PrimalSimplex(IIterationLogger logger, double bigM = 1e6, double eps = 1e-9)
+        {
+            _log = logger;
+            _M = bigM;
+            _eps = eps;
+        }
+
+        public SolveResult Solve(CanonicalForm cf)
+        {
+            _log.LogHeader("Primal Simplex (Tableau)");
+            var res = new SolveResult();
+            try
+            {
+                BuildTableau(cf, out var T, out var basis, out var varNames, out int objRow, out int rhsCol);
+                PrintTableau(T, objRow, rhsCol, varNames, basis);
+
+                int it = 0;
+                while (true)
+                {
+                    it++;
+                    int enter = ChooseEntering(T, objRow);
+                    if (enter < 0) break; // optimal
+
+                    int leave = ChooseLeaving(T, enter, rhsCol, out bool unbounded);
+                    if (unbounded)
+                    {
+                        res.Status = "Unbounded";
+                        _log.Log("Detected unbounded: no positive entry in entering column.");
+                        return res;
+                    }
+
+                    Pivot(T, leave, enter);
+                    basis[leave] = enter;
+                    _log.Log($"Pivot: Row {leave} leaves, Col {enter} enters.");
+                    PrintTableau(T, objRow, rhsCol, varNames, basis);
+                    if (it > 10000) throw new Exception("Iteration limit exceeded.");
+                }
+
+                res.Status = "Optimal";
+                res.Iterations = it;
+                var x = new double[varNames.Length - 1];
+                for (int i = 0; i < basis.Length; i++)
+                {
+                    int col = basis[i];
+                    if (col < x.Length) x[col] = T[i, rhsCol];
+                }
+
+                double z = T[objRow, rhsCol];
+                if (cf.Sense == ProblemSense.Min) z = -z;
+
+                if (HasPositiveArtificial(varNames, basis, out int artIndex))
+                {
+                    res.Status = "Infeasible";
+                    _log.Log("Artificial variable remains in basis at positive level -> Infeasible.");
+                }
+
+                res.X = x.Select(v => Math.Round(v, 3)).ToArray();
+                res.Objective = Math.Round(z, 3);
+                _log.Log($"Final Solution: Status={res.Status}, Z={res.Objective:0.###}");
+                for (int i = 0; i < x.Length; i++) _log.Log($"x{i+1} = {res.X[i]:0.###}");
+                return res;
+            }
+            catch (Exception ex)
+            {
+                _log.Log("Error in PrimalSimplex: " + ex.Message);
+                res.Status = "Error";
+                return res;
+            }
+        }
+
     }
 }
